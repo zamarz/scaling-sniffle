71b66492d5d391728cd304727e9dea8409cf0bb3
zamarz <zachmarzouk@gmail.com>
2023-11-10 15:26:54 +0000

    "Add link to past events from event page"

--- archive-event.php
+++ archive-event.php
@@ -42,6 +42,10 @@ echo paginate_links();
 
 ?>
 
+<hr class="section-break">
+
+<p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a></p>
+
 </div>
 
 
