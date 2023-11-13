a9f4b793f47419de22738ba445c59a007ef5ef5e
zamarz <zachmarzouk@gmail.com>
2023-11-13 12:44:46 +0000

    "Add more info to program and professor page"

--- single-program.php
+++ single-program.php
@@ -1,5 +1,4 @@
 <?php 
-
 get_header();
 
 while(have_posts()) {
@@ -26,6 +25,35 @@ while(have_posts()) {
     </div>
 
     <?php
+    $relatedProfessors = new WP_Query(array(
+      'posts_per_page' => -1,
+      'post_type' => 'professor',
+      'orderby' => 'title',
+      'order' => 'ASC',
+      "meta_query" => array(
+        array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . get_the_ID() . '"'
+        )
+      )
+      ));
+
+      if ($relatedProfessors->have_posts()) {
+        echo '<hr class="section-break">';  
+      echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+      while($relatedProfessors->have_posts()) {
+        $relatedProfessors->the_post(); ?>
+          
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><li>
+        <?php  }
+      }
+
+      wp_reset_postdata();
+      //resets global post object back to default, URL based query
+
+
       $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
