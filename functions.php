9303307afd0361eec3e5d88718fbe0f00f47830f
zamarz <zachmarzouk@gmail.com>
2023-11-13 15:20:54 +0000

    Add page banner and subtitle

--- functions.php
+++ functions.php
@@ -17,6 +17,8 @@ function university_features() {
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
+
 
 }
 
