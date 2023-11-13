1203ad0eba9448b40dc32382e80a4cefb9cef136
zamarz <zachmarzouk@gmail.com>
2023-11-13 13:12:16 +0000

    Add new function for image sizes

--- functions.php
+++ functions.php
@@ -15,6 +15,8 @@ function university_features() {
 
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
 
 }
 
