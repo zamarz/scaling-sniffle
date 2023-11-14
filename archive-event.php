584d676aad84ac861c155eb90c2fe11b8b26ffe1
zamarz <zachmarzouk@gmail.com>
2023-11-14 11:40:39 +0000

    Create event template and implement across site

--- archive-event.php
+++ archive-event.php
@@ -13,29 +13,11 @@ pageBanner(array(
 <?php
 while(have_posts()) {
 
-  the_post(); ?>
+  the_post();
+  get_template_part('template-parts/content-event');
 
-<div class="event-summary">
-<a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month"><?php
-              
-
-              $eventDate = new DateTime(get_field('event_date'));
-              echo $eventDate->format('M');
-              ?></span>
-              <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-
-  <?php
 }
-
 echo paginate_links();
-
 ?>
 
 <hr class="section-break">
@@ -43,9 +25,6 @@ echo paginate_links();
 <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a></p>
 
 </div>
-
-
-
 
 <?php get_footer();
 
