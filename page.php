82bde6348d7925420435ade3b3583b970bdebc14
zamarz <zachmarzouk@gmail.com>
2023-11-14 10:10:35 +0000

    Add image fallbacks

--- page.php
+++ page.php
@@ -3,17 +3,16 @@
 get_header();
 
 while(have_posts()) {
-    the_post(); ?>
+    the_post(); 
     
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>DON't FORGET TO REPLACE ME</p>
-        </div>
-      </div>
-    </div>
+    pageBanner(array(
+      'title' => 'Hello there this is the title',
+      'subtitle' => 'Hi this is the subtitle',
+      
+        ));
+    ?>
+    
+   
 
     <div class="container container--narrow page-section">
 
