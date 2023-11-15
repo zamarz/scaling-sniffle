02ce7dacc65656f0ae20904d09dc04f3b25d8fed
zamarz <zachmarzouk@gmail.com>
2023-11-15 16:18:46 +0000

    Add campuses to program page

--- single-program.php
+++ single-program.php
@@ -86,6 +86,20 @@ while(have_posts()) {
               get_template_part('template-parts/content-event');
             }
             }
+            wp_reset_postdata();
+            $relatedCampuses = get_field('related_campus');
+
+            if ($relatedCampuses) {
+              echo'<hr class="section-break">';
+              echo '<h2 class="headline headline--medium">' . get_the_title() .  ' is available at these campuses:</h2>';
+              
+              echo '<ul class="min-list link-list"></ul>';
+              foreach($relatedCampuses as $campus) {
+                ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li> <?php
+              }
+              echo '</ul>';
+            }
+
               ?>
     </div>
 
