1d160f787cd8d23aa2509d473ee76d1bfd4eed49
zamarz <zachmarzouk@gmail.com>
2023-11-23 13:06:34 +0000

    Improve search

--- search.php
+++ search.php
@@ -11,36 +11,27 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
 <?php
-while(have_posts()) {
 
-  the_post(); 
-  get_template_part('/template-parts/content', get_post_type());
-  
-  ?>
+if(have_posts()) {
+    while(have_posts()) {
 
-  <div class="post-item">
-    <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <div class="metabox">
-    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-    </div>
-    </div>
+        the_post(); 
+        get_template_part('/template-parts/content', get_post_type());
+        
+      
+      }
+      
+      echo paginate_links();
 
-    <div class="generic-content">
-    <?php the_excerpt(); ?>
-    <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-    </div>
-
-  <?php
+} else {
+    echo '<h2 class="headline headline--small-plus">No results match that search</h2>';
 }
 
-echo paginate_links();
+get_search_form();
 
 ?>
 
 </div>
-
-
-
 
 <?php get_footer();
 
