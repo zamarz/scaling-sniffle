a22996d736459e902ee5544c1b66d84453c9f8e9
zamarz <zachmarzouk@gmail.com>
2023-11-23 10:37:07 +0000

    Ensure events work correctly in search

--- inc/search-route.php
+++ inc/search-route.php
@@ -98,7 +98,7 @@ if ($results['programs']) {
     }
     
     $programRelationshipQuery = new WP_Query(array(
-        'post_type' => 'professor',
+        'post_type' => array('professor', 'event'),
         'meta_query' => $programsMetaQuery
     ));
     
@@ -113,9 +113,30 @@ if ($results['programs']) {
                 'image' => get_the_post_thumbnail_url(0, 'professorLandscape') 
             ));
         }
+        if (get_post_type() == 'event') {
+
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+            if(has_excerpt()) {
+                $description = get_the_excerpt();
+              } else {
+                $description = wp_trim_words(get_the_content(), 18);
+              }
+    
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => $description
+            ));
+        }
+
     }
     
         $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+
 
 
 }
