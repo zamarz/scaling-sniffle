aae7a80629cca593cdd64d28909af25d4046e19d
zamarz <zachmarzouk@gmail.com>
2023-11-23 16:23:43 +0000

    Customise login page

--- functions.php
+++ functions.php
@@ -146,4 +146,28 @@ function noSubsAdminBar() {
   }
 }
 
+//customise login screen
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderURL() {
+
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
+}
 ?>
