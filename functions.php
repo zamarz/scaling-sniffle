c7b6fd2c642770fce9582bdffebd2bbca30f204a
zamarz <zachmarzouk@gmail.com>
2023-11-23 16:12:42 +0000

    Redirect subscribers to homepage instead of admin

--- functions.php
+++ functions.php
@@ -122,4 +122,28 @@ function universityMapKey($api) {
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+
+//Redirect subscriber accounts out of admin and to homepage
+
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+  
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+  
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
 ?>
