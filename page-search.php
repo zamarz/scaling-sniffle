b8563b80db31e58ca26e61833d277aa0cf6432c7
zamarz <zachmarzouk@gmail.com>
2023-11-23 12:41:10 +0000

    Manual search customisation

--- page-search.php
+++ page-search.php
@@ -64,9 +64,12 @@ while(have_posts()) {
 
 
       <div class="generic-content">
-        <form method="get" action="<?php echo esc_url(site_url('/')) ; ?>" >
-          <input type="search" name="s">
-          <input type="submit" value="Search">
+        <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')) ; ?>" >
+        <label class="headline headline--medium" for="s">Perform a new search: </label>
+          <div class="search-form-row">
+            <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
+            <input class="search-submit" type="submit" value="Search">
+          </div>
         </form>
       </div>
     </div>
