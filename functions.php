5702eb76bcc59ddfb2865e8f635d4fd2d82fdbcf
zamarz <zachmarzouk@gmail.com>
2023-11-28 15:45:47 +0000

    Set post limit for notes

--- functions.php
+++ functions.php
@@ -175,11 +175,16 @@ function ourLoginTitle() {
 
 //Force note posts to be private
 
-add_filter('wp_insert_post_data', 'makeNotePrivate');
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
-function makeNotePrivate($data) {
+function makeNotePrivate($data, $postarr) {
 
   if ($data['post_type'] == 'note') {
+
+    if (count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
+      wp_die('You have reached your note limit');
+    }
+
     $data['post_content'] = sanitize_textarea_field($data['post_content']);
     $data['post_title'] = sanitize_text_field($data['post_title']);
 
